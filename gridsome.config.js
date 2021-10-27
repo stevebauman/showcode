ae07825809a947e4b5c965d60ff842dded3e025e
Steve Bauman <6421846+stevebauman@users.noreply.github.com>
2021-10-27 17:42:52 -0400

    Remove additional, unused config options

--- gridsome.config.js
+++ gridsome.config.js
@@ -15,12 +15,8 @@ module.exports = {
   plugins: [
     {
       use: "gridsome-plugin-tailwindcss",
-
       options: {
         tailwindConfig: './tailwind.config.js',
-        presetEnvConfig: {},
-        shouldImport: false,
-        shouldTimeTravel: false
       }
     },
   ]
