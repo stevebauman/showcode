5d1c59c2ccaf98b8c54a149c4e5143e0e43ed12b
Steve Bauman <steven_bauman@outlook.com>
2021-12-02 09:19:59 -0500

    Try to work with Dusk

--- testing/routes/web.php
+++ testing/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return File::get(public_path() . '/index.html');
 });
