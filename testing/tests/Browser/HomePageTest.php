c05ac7f942fc7d02b32352e8347ab4f43ac05c57
Steve Bauman <steven_bauman@outlook.com>
2021-12-02 08:49:33 -0500

    Giving dusk a shot

---
+++ testing/tests/Browser/HomePageTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class HomePageTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testPageLoads()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->assertSee('BACKGROUNDS');
+        });
+    }
+}
