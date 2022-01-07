929669a70d7c0817016b9c2a435b49c4a370060d
Steve Bauman <steven_bauman@outlook.com>
2022-01-07 09:44:46 -0500

    Feature flag custom backgrounds for now

--- components/Preview.vue
+++ components/Preview.vue
@@ -10,7 +10,7 @@
                     type="button"
                     dusk="button-copy"
                     @click="copyToClipboard"
-                    class="inline-flex items-center h-full gap-2 px-4 py-2 rounded-lg cursor-pointer text-ui-gray-400 bg-ui-gray-800 hover:bg-ui-gray-900 focus:bg-ui-gray-900 focus:outline-none focus:ring-2 focus:ring-ui-focus"
+                    class="inline-flex items-center h-full gap-2 px-4 py-2 rounded-lg cursor-pointer  text-ui-gray-400 bg-ui-gray-800 hover:bg-ui-gray-900 focus:bg-ui-gray-900 focus:outline-none focus:ring-2 focus:ring-ui-focus"
                 >
                     <CheckIcon v-if="copied" class="text-green-300" />
                     <ClipboardIcon v-else class="w-4 h-4" />
@@ -59,25 +59,25 @@
                             <ButtonResize
                                 data-hide
                                 v-dragged="resizeFromTop"
-                                class="absolute top-0 z-20 -mt-1 rounded-full left-1/2 cursor-resize-height"
+                                class="absolute top-0 z-20 -mt-1 rounded-full  left-1/2 cursor-resize-height"
                             />
 
                             <ButtonResize
                                 data-hide
                                 v-dragged="resizeFromBottom"
-                                class="absolute bottom-0 z-20 -mb-1 rounded-full left-1/2 cursor-resize-height"
+                                class="absolute bottom-0 z-20 -mb-1 rounded-full  left-1/2 cursor-resize-height"
                             />
 
                             <ButtonResize
                                 data-hide
                                 v-dragged="resizeFromLeft"
-                                class="absolute left-0 z-20 -ml-1 rounded-full top-1/2 cursor-resize-width"
+                                class="absolute left-0 z-20 -ml-1 rounded-full  top-1/2 cursor-resize-width"
                             />
 
                             <ButtonResize
                                 data-hide
                                 v-dragged="resizeFromRight"
-                                class="absolute right-0 z-20 -mr-1 rounded-full top-1/2 cursor-resize-width"
+                                class="absolute right-0 z-20 -mr-1 rounded-full  top-1/2 cursor-resize-width"
                             />
                         </div>
 
@@ -110,10 +110,10 @@
                         <template #title>
                             Backgrounds
 
-                            <div class="absolute right-0 mr-2 inset-y">
+                            <div v-if="customBackgrounds" class="absolute right-0 mr-2 inset-y">
                                 <button
                                     @click="showingBackgroundsModal = true"
-                                    class="h-full bg-ui-gray-800 hover:bg-ui-gray-900 rounded-xl focus:outline-none focus:ring-2 focus:ring-ui-focus"
+                                    class="h-full  bg-ui-gray-800 hover:bg-ui-gray-900 rounded-xl focus:outline-none focus:ring-2 focus:ring-ui-focus"
                                 >
                                     <PlusCircleIcon class="w-4 h-4 text-ui-gray-500" />
                                 </button>
@@ -399,6 +399,7 @@ export default {
             exportAs: 'png',
             resizing: false,
             backgrounds: [],
+            customBackgrounds: false,
             showingBackgroundsModal: false,
             blocks: [],
             settings: {
@@ -587,7 +588,6 @@ export default {
                 // We need to make sure the element exists
                 // and it's visible before attempting to
                 // update our width and height values.
-                // See: https://stackoverflow.com/a/21696585/2708607
                 if (this.$refs.capture && this.$refs.capture.offsetParent) {
                     this.settings.height = this.$refs.capture.offsetHeight;
                     this.settings.width = this.$refs.capture.offsetWidth;
