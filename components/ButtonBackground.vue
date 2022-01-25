17eb7c05d1d94532135800a9cd3f15b5bbb5e37a
Steve Bauman <steven_bauman@outlook.com>
2022-01-24 21:57:43 -0500

    Increase selected icon size

--- components/ButtonBackground.vue
+++ components/ButtonBackground.vue
@@ -1,13 +1,13 @@
 <template>
     <button
         :class="[active ? 'border-2 border-ui-gray-200' : null]"
-        class="relative w-10 h-10 rounded  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-ui-gray-700 focus:ring-ui-focus"
+        class="relative w-10 h-10 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-ui-gray-700 focus:ring-ui-focus"
     >
         <div
             v-if="active"
-            class="absolute inline-flex items-center justify-center w-4 h-4 border rounded-full shadow-sm  bg-ui-gray-800 border-ui-gray-600 -top-2 -right-2"
+            class="absolute inline-flex items-center justify-center w-5 h-5 bg-green-400 rounded-full shadow -top-2 -right-2"
         >
-            <CheckIcon class="w-2 h-2 text-green-500" />
+            <CheckIcon class="w-4 h-4 text-white" />
         </div>
     </button>
 </template>
