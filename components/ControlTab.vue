a406d88dcb8b803e19c736e9b61e7e2d7fa5ba10
Steve Bauman <steven_bauman@outlook.com>
2022-04-12 08:42:15 -0400

    Align hover background style

--- components/ControlTab.vue
+++ components/ControlTab.vue
@@ -2,7 +2,7 @@
     <button
         :class="{
             'text-ui-gray-50 bg-ui-gray-700 bg-opacity-60': active,
-            'text-ui-gray-500 bg-ui-gray-800 bg-opacity-60 hover:bg-ui-gray-600 hover:text-ui-gray-400':
+            'text-ui-gray-500 bg-ui-gray-800 bg-opacity-60 hover:bg-ui-gray-900 hover:text-ui-gray-400':
                 !active,
         }"
         class="flex items-center justify-center w-full h-8 gap-2 px-2 text-xs font-semibold leading-none tracking-widest uppercase"
