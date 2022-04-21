e49e8f04335603638904d52e13c82633b439e184
Steve Bauman <steven_bauman@outlook.com>
2022-04-20 23:07:07 -0400

    Add IPC fake

--- plugins/ipc.js
+++ plugins/ipc.js
@@ -1,17 +1,3 @@
 export default function (context, inject) {
-    const ipc = {
-        instance: null,
-
-        send(...args) {
-            if (context.$config.isDesktop) {
-                this.getInstance().send(...args);
-            }
-        },
-
-        getInstance() {
-            return this.instance ?? (this.instance = require('electron').ipcRenderer);
-        },
-    };
-
-    inject('ipc', ipc);
+    inject('ipc', require('electron').ipcRenderer);
 }
