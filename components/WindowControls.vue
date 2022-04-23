ba334bbdc6be78a74093dc27136f35391e4fe610
Steve Bauman <steven_bauman@outlook.com>
2022-04-23 11:06:08 -0400

    Update WindowControls.vue

--- components/WindowControls.vue
+++ components/WindowControls.vue
@@ -40,7 +40,13 @@
             </svg>
         </button>
 
-        <button aria-label="close" title="Close" tabindex="-1" class="window-control close">
+        <button
+            @click="$ipc.send('close')"
+            aria-label="close"
+            title="Close"
+            tabindex="-1"
+            class="window-control close"
+        >
             <svg aria-hidden="true" version="1.1" width="10" height="10">
                 <path
                     d="M 0,0 0,0.7 4.3,5 0,9.3 0,10 0.7,10 5,5.7 9.3,10 10,10 10,9.3 5.7,5 10,0.7 10,0 9.3,0 5,4.3 0.7,0 Z"
