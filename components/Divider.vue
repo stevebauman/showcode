eaf2fb43b4bbda305447b42c1283923d743dd647
Steve Bauman <steven_bauman@outlook.com>
2022-05-14 12:56:54 -0400

    Always break px on separate line

--- components/Divider.vue
+++ components/Divider.vue
@@ -2,7 +2,11 @@
     <div class="flex-col items-center justify-center hidden w-10 h-full lg:flex">
         <span class="block w-px h-full bg-ui-gray-500 strikethrough"> </span>
 
-        <div class="py-2 tracking-widest text-center uppercase z-[2]">{{ title }}</div>
+        <div class="py-2 tracking-widest text-center uppercase z-[2]">
+            {{ number }}
+            <br />
+            px
+        </div>
 
         <span class="block w-px h-full bg-ui-gray-500 strikethrough"> </span>
     </div>
@@ -10,6 +14,6 @@
 
 <script>
 export default {
-    props: { title: String },
+    props: ['number'],
 };
 </script>
