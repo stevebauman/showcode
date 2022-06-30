8953aea29247d43a2cd4eb632963c62bd0a70583
Steve Bauman <steven_bauman@outlook.com>
2022-06-30 09:23:10 -0400

    Fix localstorage restoration

--- composables/useProjectStoreFactory.js
+++ composables/useProjectStoreFactory.js
@@ -3,46 +3,49 @@ import { v4 as uuid } from 'uuid';
 import { cloneDeep, replace } from 'lodash';
 import { defineStore } from 'pinia';
 import { namespace } from './useProjectStores';
+import { useLocalStorage } from '@vueuse/core';
 import useTemplateStore from './useTemplateStore';
-import { nextTick } from '@nuxtjs/composition-api';
-import { useLocalStorage, RemovableRef } from '@vueuse/core';
 
 export default function (id) {
+    const storage = useLocalStorage(id, {
+        version: '1.9.0',
+        page: {},
+        settings: {},
+        tab: {
+            order: 0,
+            name: null,
+            created_at: new Date(),
+            id: replace(id, namespace, ''),
+        },
+    });
+
     return defineStore(id, {
-        state: () => ({
-            version: '1.9.0',
-            page: {},
-            settings: {},
-            tab: {
-                order: 0,
-                name: null,
-                created_at: new Date(),
-                id: replace(id, namespace, ''),
-            },
-        }),
+        state: () => storage.value,
 
         actions: {
             /**
              * Sync the state into local storage.
              */
             sync() {
-                this.storage().value = cloneDeep(this.$state);
+                if (storage.value !== null) {
+                    storage.value = cloneDeep(this.$state);
+                }
             },
 
             /**
              * Load the state from local storage.
              */
             load() {
-                Object.assign(this.$state, this.storage().value);
+                Object.assign(this.$state, storage.value);
             },
 
             /**
              * Clear the state from local storage.
              */
             clear() {
-                this.storage().value = null;
+                storage.value = null;
 
-                nextTick(() => window.localStorage.removeItem(`${namespace}/${this.tab.id}`));
+                window.localStorage.removeItem(`${namespace}/${this.tab.id}`);
             },
 
             /**
@@ -56,15 +59,6 @@ export default function (id) {
                 clone.tab.id = uuid();
 
                 return clone;
-            },
-
-            /**
-             * Get the local storage ref.
-             *
-             * @returns {RemovableRef}
-             */
-            storage() {
-                return useLocalStorage(id, cloneDeep(this.$state));
             },
 
             /**
