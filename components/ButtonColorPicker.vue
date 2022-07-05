f3b7998a8ac15fd696b2f4d1242d0072a755060a
Steve Bauman <steven_bauman@outlook.com>
2022-07-04 23:10:05 -0400

    Re-add ability to ender raw hex and RGBA

--- components/ButtonColorPicker.vue
+++ components/ButtonColorPicker.vue
@@ -12,11 +12,16 @@
 
         <template #popover>
             <Chrome
-                disable-fields
                 :value="{ r: value.red, g: value.green, b: value.blue, a: value.alpha }"
                 @input="
-                    ({ rgba }) =>
-                        $emit('change', { red: rgba.r, green: rgba.g, blue: rgba.b, alpha: rgba.a })
+                    ({ rgba }) => {
+                        $emit('change', {
+                            red: rgba.r,
+                            green: rgba.g,
+                            blue: rgba.b,
+                            alpha: rgba.a,
+                        });
+                    }
                 "
             />
         </template>
@@ -25,21 +30,55 @@
 
 <style>
 .vc-chrome {
-    border-radius: 0.5rem;
-    box-shadow: none !important;
-    @apply overflow-hidden rounded-lg;
+    @apply rounded-lg !shadow-none;
 }
 
 .vc-chrome-body {
-    background-color: var(--color-ui-gray-700) !important;
-}
-
-.vc-chrome-saturation-wrap {
-    @apply bg-ui-gray-700;
+    @apply !bg-ui-gray-700;
 }
 
 .vc-saturation {
-    @apply m-2 rounded-lg overflow-hidden;
+    @apply m-2 rounded-lg;
+}
+
+.vc-chrome-saturation-wrap {
+    @apply p-6 !bg-ui-gray-700;
+}
+
+.vc-saturation--white {
+    @apply rounded-lg;
+}
+
+.vc-saturation--black {
+    @apply rounded-lg;
+}
+
+.vc-chrome-fields-wrap {
+    @apply space-x-2;
+}
+
+.vc-chrome-fields .vc-input__input {
+    @apply !border-0 !rounded-lg !shadow-none !text-ui-gray-400 !bg-ui-gray-600 hover:bg-ui-gray-900 focus:outline-none focus:bg-ui-gray-900 focus:ring-2 focus:ring-ui-focus;
+}
+
+.vc-chrome-fields .vc-input__label {
+    @apply text-xs font-semibold text-ui-gray-300 whitespace-nowrap;
+}
+
+.vc-chrome-toggle-btn {
+    @apply flex items-center !text-ui-gray-300 rounded-lg justify-center transition duration-100 ease-in-out bg-ui-gray-700 hover:bg-ui-gray-900;
+}
+
+.vc-chrome-toggle-icon {
+    @apply !m-0 inline-flex items-center gap-2 leading-none transition duration-100 ease-in-out focus:outline-none focus:ring-2 focus:ring-ui-focus;
+}
+
+.vc-chrome-toggle-icon svg path {
+    fill: currentColor !important;
+}
+
+.vc-chrome-toggle-icon-highlight {
+    @apply hidden;
 }
 </style>
 
