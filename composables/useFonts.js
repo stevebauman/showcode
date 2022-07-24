8ef36f72eb79b86b74d2fbe8ab6a1f53ecff1f8a
Steve Bauman <steven_bauman@outlook.com>
2022-07-24 14:45:45 -0400

    Add additional font sizes

--- composables/useFonts.js
+++ composables/useFonts.js
@@ -4,7 +4,7 @@ import { onMounted, ref, useContext } from '@nuxtjs/composition-api';
 export default function () {
     const { $ipc } = useContext();
 
-    const fontSizes = ref([12, 14, 16, 18, 20]);
+    const fontSizes = ref([6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 30]);
 
     const fontFamilies = ref([
         {
