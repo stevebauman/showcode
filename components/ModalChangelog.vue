40a70ab792573a50ec9cd3ea1d1ddf7e738979b8
Steve Bauman <steven_bauman@outlook.com>
2022-10-02 18:42:26 -0400

    Remove overflow wraps

--- components/ModalChangelog.vue
+++ components/ModalChangelog.vue
@@ -1,8 +1,6 @@
 <template>
     <Modal v-bind="$attrs" v-on="$listeners" size="sm" class="space-y-4" header="Changelog">
-        <div class="overflow-y-scroll max-h-[40rem] rounded-lg">
-            <nuxt-content :document="content" />
-        </div>
+        <nuxt-content :document="content" />
     </Modal>
 </template>
 
