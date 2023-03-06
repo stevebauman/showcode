07213f77f401988cbab07dbce8f5ee75184c5dc4
Steve Bauman <steven_bauman@outlook.com>
2023-03-06 16:09:09 -0500

    Bump version

--- composables/useProjectStoreFactory.js
+++ composables/useProjectStoreFactory.js
@@ -10,7 +10,7 @@ export default function (id, initialValue = null) {
     const storage = useIndexedDb(
         id,
         initialValue ?? {
-            version: '1.18.6',
+            version: '1.19.0',
             modified: false,
             page: {},
             settings: {},
