ba95f316a4787a5a68cc496f809d20c0b595504a
Steve Bauman <steven_bauman@outlook.com>
2024-01-01 17:02:39 -0500

    Update useProjectStoreFactory.js

--- composables/useProjectStoreFactory.js
+++ composables/useProjectStoreFactory.js
@@ -11,7 +11,7 @@ export default function (id, initialValue = null) {
     const storage = useIndexedDb(
         id,
         initialValue ?? {
-            version: '1.20.2',
+            version: '1.20.3',
             modified: false,
             page: {},
             settings: {},
