8dbd6f6193efcf84fef1fdb20779ee73f1cf214b
Steve Bauman <steven_bauman@outlook.com>
2024-04-28 14:17:18 -0400

    Update ToggleHeader.vue

--- components/ToggleHeader.vue
+++ components/ToggleHeader.vue
@@ -7,7 +7,9 @@
     >
         <template #popover>
             <div class="grid grid-cols-2 grid-rows-2">
-                <div class="flex items-center justify-between border-r border-b gap-2 p-2">
+                <div
+                    class="flex items-center justify-between border-r border-b border-ui-gray-800 gap-2 p-2"
+                >
                     <Label> Title </Label>
 
                     <div class="flex items-center">
@@ -19,7 +21,9 @@
                     </div>
                 </div>
 
-                <div class="flex items-center justify-between border-b gap-2 p-2">
+                <div
+                    class="flex items-center justify-between border-b border-ui-gray-800 gap-2 p-2"
+                >
                     <Label> Accent </Label>
 
                     <div class="flex items-center">
@@ -31,7 +35,9 @@
                     </div>
                 </div>
 
-                <div class="flex items-center justify-between border-r gap-2 p-2">
+                <div
+                    class="flex items-center justify-between border-r border-ui-gray-800 gap-2 p-2"
+                >
                     <Label class="whitespace-nowrap"> Menu </Label>
 
                     <div class="flex items-center">
