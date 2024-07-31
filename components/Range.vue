fb99b8690e7cd7904e85c152b99d6351beb2cb2e
Steve Bauman <steven_bauman@outlook.com>
2024-07-30 22:51:15 -0400

    Fix input range slider on chrome

--- components/Range.vue
+++ components/Range.vue
@@ -1,7 +1,7 @@
 <template>
     <input
-        :value="value"
         type="range"
+        :value="value"
         @input="$emit('input', $event.target.value)"
         class="px-0.5 transition-all highlight bg-ui-gray-800 w-full appearance-none hover:bg-ui-violet-500 rounded-xl focus:bg-ui-violet-500 focus:outline-none focus:ring-0"
     />
@@ -16,12 +16,16 @@ export default {
 </script>
 
 <style scoped>
+input[type='range']::-webkit-slider-runnable-track {
+    @apply h-5 bg-ui-gray-800 rounded-full hover:bg-ui-violet-500 transition px-0.5;
+}
+
 input[type='range']::-webkit-slider-thumb {
-    @apply h-4 w-4 my-0.5 rounded-full appearance-none cursor-resize-width border-white bg-white;
+    @apply size-4 my-0.5 rounded-full shadow appearance-none cursor-resize-width border-white bg-white;
 }
 
 input[type='range']::-moz-range-thumb {
-    @apply h-4 w-4 my-0.5 rounded-full appearance-none cursor-resize-width border-white bg-white;
+    @apply size-4 my-0.5 rounded-full appearance-none cursor-resize-width border-white bg-white;
 }
 
 input[type='range']:disabled::-webkit-slider-thumb {
