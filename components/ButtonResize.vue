ceace8b800089dcf5312a3d9861f51c2b0637124
Steve Bauman <steven_bauman@outlook.com>
2026-03-24 21:46:39 -0400

    Add dark/light mode

--- components/ButtonResize.vue
+++ components/ButtonResize.vue
@@ -2,7 +2,7 @@
     <button
        
         :style="{ transform: `scale(${zoomScale})` }"
-        class="z-20 bg-white border border-gray-700 rounded-full shadow exclude-from-panzoom active:bg-zinc-100 dark:active:bg-zinc-900 hover:scale-200"
+        class="z-20 bg-white dark:bg-zinc-800 border border-zinc-400 dark:border-zinc-500 rounded-full shadow exclude-from-panzoom active:bg-zinc-100 dark:active:bg-zinc-900 hover:scale-200"
     ></button>
 </template>
 
