a22b92b9f8857d3cc99b52579cd3b0f0500d1d42
Steve Bauman <steven_bauman@outlook.com>
2026-03-29 00:04:05 -0400

    Setup favicon and remove e2e tests

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,6 +40,13 @@ export default defineNuxtConfig({
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
                 { name: 'description', content: 'Create beautiful images of code.' },
             ],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+                { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+            ],
         },
     },
 })
--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,6 @@ import { defineVitestConfig } from '@nuxt/test-utils/config';
 
 export default defineVitestConfig({
     test: {
-        exclude: ['tests/e2e/**', 'node_modules/**'],
         environment: 'nuxt',
         environmentOptions: {
             nuxt: {
