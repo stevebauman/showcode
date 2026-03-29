8465ca58d2a53bf33d413132e72f04f4f03f8bf6
Steve Bauman <steven_bauman@outlook.com>
2026-03-29 00:14:35 -0400

    Fix popover background

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -286,6 +286,26 @@ body,
     @apply hidden;
 }
 
+.v-popper--theme-dropdown .v-popper__inner {
+    @apply overflow-hidden rounded-lg border border-zinc-200 bg-zinc-100 shadow-xl;
+}
+
+.dark .v-popper--theme-dropdown .v-popper__inner {
+    @apply border-zinc-800 bg-zinc-900;
+}
+
+.v-popper--theme-dropdown .v-popper__arrow-outer {
+    @apply border-zinc-200;
+}
+
+.dark .v-popper--theme-dropdown .v-popper__arrow-outer {
+    @apply border-zinc-800;
+}
+
+.v-popper--theme-dropdown .v-popper__arrow-inner {
+    @apply hidden;
+}
+
 .bg-pattern {
     background-position: 0.5% 0;
     background-image: url("data:image/svg+xml,%3Csvg width='32' height='32' viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 16c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zm33.414-6l5.95-5.95L45.95.636 40 6.586 34.05.636 32.636 2.05 38.586 8l-5.95 5.95 1.414 1.414L40 9.414l5.95 5.95 1.414-1.414L41.414 8zM40 48c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zM9.414 40l5.95-5.95-1.414-1.414L8 38.586l-5.95-5.95L.636 34.05 6.586 40l-5.95 5.95 1.414 1.414L8 41.414l5.95 5.95 1.414-1.414L9.414 40z' fill='%239C92AC' fill-opacity='0.15' fill-rule='evenodd'/%3E%3C/svg%3E");
