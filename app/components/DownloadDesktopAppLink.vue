c4324656607b55bb7a8e0296e8fd5ad48e85bf30
Steve Bauman <steven_bauman@outlook.com>
2026-08-17 10:03:21 -0400

    Only show text when hovered

--- app/components/DownloadDesktopAppLink.vue
+++ app/components/DownloadDesktopAppLink.vue
@@ -1,14 +1,18 @@
 <template>
-    <a
-        v-show="visible"
-        ref="link"
-        :href="href"
-        target="_blank"
-        class="mr-1 flex h-8 shrink-0 items-center gap-1.5 rounded-lg px-2.5 text-xs font-medium text-zinc-500 transition-colors hover:bg-zinc-200/50 hover:text-zinc-700 dark:text-zinc-400 dark:hover:bg-zinc-800/50 dark:hover:text-zinc-200"
-    >
-        <MonitorDownIcon class="size-3.5" />
-        Get Desktop App
-    </a>
+    <div v-show="visible" ref="link" class="relative mr-1 h-8 w-8 shrink-0">
+        <a
+            :href="href"
+            target="_blank"
+            class="group absolute top-0 right-0 z-30 flex h-8 w-8 items-center overflow-hidden rounded-lg px-[9px] text-xs font-medium whitespace-nowrap text-zinc-500 transition-[width,color,background-color] duration-200 ease-out hover:w-36 hover:bg-zinc-200/50 hover:text-zinc-700 focus-visible:w-36 focus-visible:ring-2 focus-visible:ring-violet-500 focus-visible:outline-none dark:text-zinc-400 dark:hover:bg-zinc-800/50 dark:hover:text-zinc-200"
+        >
+            <MonitorDownIcon class="size-3.5 shrink-0" />
+            <span
+                class="ml-1.5 shrink-0 -translate-x-1 opacity-0 transition-[opacity,transform] duration-150 group-hover:translate-x-0 group-hover:opacity-100 group-focus-visible:translate-x-0 group-focus-visible:opacity-100"
+            >
+                Get Desktop App
+            </span>
+        </a>
+    </div>
 </template>
 
 <script setup>
